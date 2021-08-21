9e3ca932649525e798da23614ca0ef9ec6712ad4
Nav <nav@navnav.me>
2021-08-21 21:19:04 +0100

    Updated build script to use Qt6 libraries

--- build/scripts/CleanBuild.sh
+++ build/scripts/CleanBuild.sh
@@ -8,7 +8,7 @@ rm -fr release;
 rm -fr Bloom-*.deb;
 rm -fr "_CPack_Packages";
 
-export CMAKE_PREFIX_PATH=/opt/Qt/5.12.10/gcc_64/
+export CMAKE_PREFIX_PATH=/opt/Qt/6.1.2/gcc_64/
 
 cd build/cmake-build-release/ && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/usr/local/install/bin/g++ ../../
 cd /home/nav/Projects/Bloom/ && cmake --build /home/nav/Projects/Bloom/build/cmake-build-release --target clean
