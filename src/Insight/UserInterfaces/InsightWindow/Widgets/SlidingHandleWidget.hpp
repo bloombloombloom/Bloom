5e280f93273c36020ee9d1b4a66cba90ce35b5fe
Nav <nav@navnav.me>
2021-08-22 20:46:19 +0100

    New sliding handle widget for resizing side panels

---
+++ src/Insight/UserInterfaces/InsightWindow/Widgets/SlidingHandleWidget.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <QFrame>
+#include <QSize>
+#include <QEvent>
+#include <QMouseEvent>
+#include <QEnterEvent>
+
+#include "src/Logger/Logger.hpp"
+
+namespace Bloom::Widgets
+{
+    class Q_WIDGETS_EXPORT SlidingHandleWidget: public QFrame
+    {
+    Q_OBJECT
+        Q_PROPERTY(int handleWidth READ getHandleWidth WRITE setHandleWidth DESIGNABLE true)
+
+    protected:
+        int handleWidth = 10;
+
+        void mouseMoveEvent(QMouseEvent* event) override;
+        void enterEvent(QEnterEvent* event) override;
+        void leaveEvent(QEvent* event) override;
+
+    public:
+        explicit SlidingHandleWidget(QWidget* parent): QFrame(parent) {};
+
+        QSize minimumSizeHint() const override {
+            return QSize(this->handleWidth, this->parentWidget()->height());
+        };
+
+        QSize sizeHint() const override {
+            return QSize(this->handleWidth, this->parentWidget()->height());
+        };
+
+        void setHandleWidth(int handleWidth) {
+            this->handleWidth = handleWidth;
+            this->setFixedWidth(handleWidth);
+        }
+
+        int getHandleWidth() {
+            return this->handleWidth;
+        }
+
+    signals:
+        void horizontalSlide(int position);
+
+    };
+}
