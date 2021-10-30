58f994bba4fc821e1cb83cd4403233c1780297ae
Nav <nav@navnav.me>
2021-10-30 21:08:51 +0100

    Bumped version number for bug fix release (v0.4.2).

--- src/Application.hpp
+++ src/Application.hpp
@@ -29,7 +29,7 @@ namespace Bloom
     class Application: public Thread
     {
     public:
-        static const inline std::string VERSION_STR = "0.4.1";
+        static const inline std::string VERSION_STR = "0.4.2";
 
     private:
         /**
