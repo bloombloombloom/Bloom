9bbc534973cc983bfecf42265151fc8466a7c327
Nav <nav@navnav.me>
2022-02-04 22:19:57 +0000

    Adjusted manage memory region window size

--- src/Insight/UserInterfaces/InsightWindow/Widgets/TargetMemoryInspectionPane/MemoryRegionManager/MemoryRegionManagerWindow.cpp
+++ src/Insight/UserInterfaces/InsightWindow/Widgets/TargetMemoryInspectionPane/MemoryRegionManager/MemoryRegionManagerWindow.cpp
@@ -56,7 +56,7 @@ MemoryRegionManagerWindow::MemoryRegionManagerWindow(
     }
 
     this->setStyleSheet(windowStylesheet.readAll());
-    this->setFixedSize(QSize(900, 520));
+    this->setFixedSize(QSize(970, 540));
 
     auto uiLoader = UiLoader(this);
     this->container = uiLoader.load(&windowUiFile, this);
