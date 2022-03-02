a9fe05e802b92d0d3bd63fe2d6f656eb402b6c66
Nav <nav@navnav.me>
2022-03-02 22:45:29 +0000

    EDBG AVR command frames for the AVRISP sub-protocol.

---
+++ src/DebugToolDrivers/Protocols/CMSIS-DAP/VendorSpecific/EDBG/AVR/CommandFrames/AVRISP/ReadFuse.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <cstdint>
+#include <src/Targets/Microchip/AVR/Fuse.hpp>
+
+#include "AvrIspCommandFrame.hpp"
+
+#include "src/Targets/Microchip/AVR/Fuse.hpp"
+
+namespace Bloom::DebugToolDrivers::Protocols::CmsisDap::Edbg::Avr::CommandFrames::AvrIsp
+{
+    class ReadFuse: public AvrIspCommandFrame<std::array<unsigned char, 6>>
+    {
+    public:
+        ReadFuse(
+            Targets::Microchip::Avr::FuseType fuseType,
+            std::uint8_t returnAddress
+        ) {
+            using Targets::Microchip::Avr::FuseType;
+
+            /*
+             * The read fuse command consists of 6 bytes:
+             *
+             * 1. Command ID (0x18)
+             * 2. Return Address
+             * 3. CMD1
+             * 4. CMD2
+             * 5. CMD3
+             * 6. CMD4
+             */
+            this->payload = {
+                0x18,
+                returnAddress,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+            };
+
+            switch (fuseType) {
+                case FuseType::LOW: {
+                    this->payload[2] = 0x50;
+                    this->payload[3] = 0x00;
+                    this->payload[4] = 0x00;
+                    this->payload[5] = 0x00;
+                    break;
+                }
+                case FuseType::HIGH: {
+                    this->payload[2] = 0x58;
+                    this->payload[3] = 0x08;
+                    this->payload[4] = 0x00;
+                    this->payload[5] = 0x00;
+                    break;
+                }
+                case FuseType::EXTENDED: {
+                    this->payload[2] = 0x50;
+                    this->payload[3] = 0x08;
+                    this->payload[4] = 0x00;
+                    this->payload[5] = 0x00;
+                    break;
+                }
+            }
+        }
+    };
+}
--- src/DebugToolDrivers/Protocols/CMSIS-DAP/VendorSpecific/EDBG/Edbg.hpp
+++ src/DebugToolDrivers/Protocols/CMSIS-DAP/VendorSpecific/EDBG/Edbg.hpp
@@ -6,6 +6,7 @@ namespace Bloom::DebugToolDrivers::Protocols::CmsisDap::Edbg
     {
         DISCOVERY = 0x00,
         HOUSE_KEEPING = 0x01,
+        AVRISP = 0x11,
         AVR8_GENERIC = 0x12,
         AVR32_GENERIC = 0x13,
     };
