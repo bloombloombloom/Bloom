97cc3cdce39885a60aa79d69d2083d981e85b990
Nav <nav@navnav.me>
2022-03-16 17:12:19 +0000

    EDBG Parameters for LED and Target Power control

--- src/DebugToolDrivers/Protocols/CMSIS-DAP/VendorSpecific/EDBG/Edbg.hpp
+++ src/DebugToolDrivers/Protocols/CMSIS-DAP/VendorSpecific/EDBG/Edbg.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 namespace Bloom::DebugToolDrivers::Protocols::CmsisDap::Edbg
 {
     enum class ProtocolHandlerId: unsigned char
@@ -11,5 +13,21 @@ namespace Bloom::DebugToolDrivers::Protocols::CmsisDap::Edbg
         AVR32_GENERIC = 0x13,
         EDBG_CONTROL = 0x20,
     };
+
+    struct EdbgParameter
+    {
+        unsigned char context = 0x00;
+        unsigned char id = 0x00;
+        std::uint8_t size = 0;
+
+        constexpr EdbgParameter() = default;
+        constexpr EdbgParameter(unsigned char context, unsigned char id, std::uint8_t size)
+            : context(context), id(id), size(size) {};
+    };
+
+    struct EdbgParameters
+    {
+        static constexpr EdbgParameter CONTROL_LED_USAGE {0x00, 0x00, 1};
+        static constexpr EdbgParameter CONTROL_TARGET_POWER {0x00, 0x10, 1};
     };
 }
