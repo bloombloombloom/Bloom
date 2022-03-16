7adfd47214e0fd1ace9cf889792969a7e7f2e965
Nav <nav@navnav.me>
2022-03-16 17:09:18 +0000

    New base class for EDBG Control protocol command frame

---
+++ src/DebugToolDrivers/Protocols/CMSIS-DAP/VendorSpecific/EDBG/AVR/CommandFrames/EDBGControl/EdbgControlCommandFrame.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "src/DebugToolDrivers/Protocols/CMSIS-DAP/VendorSpecific/EDBG/AVR/CommandFrames/AvrCommandFrame.hpp"
+#include "src/DebugToolDrivers/Protocols/CMSIS-DAP/VendorSpecific/EDBG/AVR/ResponseFrames/EDBGControl/EdbgControlResponseFrame.hpp"
+
+namespace Bloom::DebugToolDrivers::Protocols::CmsisDap::Edbg::Avr::CommandFrames::EdbgControl
+{
+    template<class PayloadContainerType>
+    class EdbgControlCommandFrame: public AvrCommandFrame<PayloadContainerType>
+    {
+    public:
+        using ExpectedResponseFrameType = ResponseFrames::EdbgControl::EdbgControlResponseFrame;
+
+        EdbgControlCommandFrame(): AvrCommandFrame<PayloadContainerType>(ProtocolHandlerId::EDBG_CONTROL) {}
+    };
+}
