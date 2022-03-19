18f08d8faa287f0084ccda35755f233514ff7768
Nav <nav@navnav.me>
2022-03-19 15:15:36 +0000

    New GdbDebugServerConfig class, for GDB specific config parameters

---
+++ src/DebugServers/GdbRsp/GdbDebugServerConfig.cpp
@@ -0,0 +1,19 @@
+#include "GdbDebugServerConfig.hpp"
+
+namespace Bloom::DebugServers::Gdb
+{
+    GdbDebugServerConfig::GdbDebugServerConfig(const DebugServerConfig& debugServerConfig)
+        : DebugServerConfig(debugServerConfig)
+    {
+        if (debugServerConfig.jsonObject.contains("ipAddress")) {
+            this->listeningAddress = debugServerConfig.jsonObject.value("ipAddress").toString().toStdString();
+        }
+
+        if (debugServerConfig.jsonObject.contains("port")) {
+            const auto portValue = debugServerConfig.jsonObject.value("port");
+            this->listeningPortNumber = static_cast<std::uint16_t>(
+                portValue.isString() ? portValue.toString().toInt(nullptr, 10) : portValue.toInt()
+            );
+        }
+    }
+}
---
+++ src/DebugServers/GdbRsp/GdbDebugServerConfig.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "src/ProjectConfig.hpp"
+
+namespace Bloom::DebugServers::Gdb
+{
+    /**
+     * Extending the generic DebugServerConfig struct to accommodate GDB debug server configuration parameters.
+     */
+    class GdbDebugServerConfig: public DebugServerConfig
+    {
+    public:
+        /**
+         * The port number for the GDB server to listen on.
+         *
+         * This parameter is optional. If not specified, the default value set here will be used.
+         */
+        std::uint16_t listeningPortNumber = 1442;
+
+        /**
+         * The address for the GDB server to listen on.
+         *
+         * This parameter is optional. If not specified, the default value set here will be used.
+         */
+        std::string listeningAddress = "127.0.0.1";
+
+        explicit GdbDebugServerConfig(const DebugServerConfig& debugServerConfig);
+    };
+}
