d65e8cf89f0cb7d485c28e43f1a6965ab5d7ad5e
Nav <nav@navnav.me>
2022-06-01 21:46:20 +0100

    Fixed AVR CMSIS command packet splitting bug

--- src/DebugToolDrivers/Protocols/CMSIS-DAP/VendorSpecific/EDBG/AVR/AvrCommand.cpp
+++ src/DebugToolDrivers/Protocols/CMSIS-DAP/VendorSpecific/EDBG/AVR/AvrCommand.cpp
@@ -6,13 +6,13 @@ namespace Bloom::DebugToolDrivers::Protocols::CmsisDap::Edbg::Avr
         std::vector<unsigned char> data;
         auto commandPacket = this->getCommandPacket();
         std::size_t commandPacketSize = commandPacket.size();
-        data.resize(3 + commandPacketSize);
+        data.reserve(3 + commandPacketSize);
         // FragmentInfo byte
-        data[0] = static_cast<unsigned char>((this->getFragmentNumber() << 4) | this->getFragmentCount());
+        data.emplace_back(static_cast<unsigned char>((this->getFragmentNumber() << 4) | this->getFragmentCount()));
 
         // Size byte
-        data[1] = static_cast<unsigned char>(commandPacketSize >> 8);
-        data[2] = static_cast<unsigned char>(commandPacketSize & 0xFF);
+        data.emplace_back(static_cast<unsigned char>(commandPacketSize >> 8));
+        data.emplace_back(static_cast<unsigned char>(commandPacketSize & 0xFF));
 
         if (commandPacketSize > 0) {
             data.insert(data.begin() + 3, commandPacket.begin(), commandPacket.end());
